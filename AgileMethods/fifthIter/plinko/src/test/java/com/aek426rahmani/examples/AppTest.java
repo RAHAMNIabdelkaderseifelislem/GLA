f77a5c2fe66665e3169457d6530949db37040559
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 21:55:24 +0100

    fifth iteration

--- AgileMethods/fifthIter/plinko/src/test/java/com/aek426rahmani/examples/AppTest.java
+++ AgileMethods/fifthIter/plinko/src/test/java/com/aek426rahmani/examples/AppTest.java
@@ -35,4 +35,85 @@ public class AppTest
     {
         assertTrue( true );
     }
+
+    /*
+     * test an app that calculates the factorial of a number
+     * number is 1
+     */
+    public void testFactorialOfOne()
+    {
+        // arrange
+        int number = 1;
+        int expected = 1;
+
+        // act
+        int actual = App.factorial(number);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * test an app that calculates the factorial of a number
+     * number is 2
+     */
+    public void testFactorialOfTwo()
+    {
+        // arrange
+        int number = 2;
+        int expected = 2;
+
+        // act
+        int actual = App.factorial(number);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * test an app that calculates the factorial of a number
+     * number is 3
+     */
+    public void testFactorialOfThree()
+    {
+        // arrange
+        int number = 3;
+        int expected = 6;
+
+        // act
+        int actual = App.factorial(number);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+
+    /*
+     * test an app that calculates the factorial of a number
+     * this time we will test the factorial of 0
+     */
+    public void testFactorialOfZero()
+    {
+        // arrange
+        int number = 0;
+        int expected = 1;
+
+        // act
+        int actual = App.factorial(number);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+    /*
+     * test an app that calculates the factorial of a number
+     * this time we will test the factorial of -1 and we will expect an exception
+     */
+    @Test(expected = NegativeNumberException.class)
+    public void testFactorialOfNegativeOne()
+    {
+        // arrange
+        int number = -1;
+
+        // act
+        App.factorial(number);
+    }
 }
