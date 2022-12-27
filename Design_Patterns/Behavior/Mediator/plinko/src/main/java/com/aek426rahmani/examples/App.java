96d59aff2c33c06dfaf7da3d014ee6f521eadf25
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 17:52:00 +0100

    Mediator code

--- Design_Patterns/Behavior/Mediator/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ Design_Patterns/Behavior/Mediator/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,121 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
+// source : https://www.geeksforgeeks.org/mediator-design-pattern/
+
+class ATCMediator implements IATCMediator
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	private Flight flight;
+	private Runway runway;
+	public boolean land;
+
+	public void registerRunway(Runway runway)
+	{
+		this.runway = runway;
+	}
+
+	public void registerFlight(Flight flight)
+	{
+		this.flight = flight;
+	}
+
+	public boolean isLandingOk()
+	{
+		return land;
+	}
+
+	@Override
+	public void setLandingStatus(boolean status)
+	{
+		land = status;
+	}
 }
+
+interface Command
+{
+	void land();
+}
+
+interface IATCMediator
+{
+
+	public void registerRunway(Runway runway);
+
+	public void registerFlight(Flight flight);
+
+	public boolean isLandingOk();
+
+	public void setLandingStatus(boolean status);
+}
+
+class Flight implements Command
+{
+	private IATCMediator atcMediator;
+
+	public Flight(IATCMediator atcMediator)
+	{
+		this.atcMediator = atcMediator;
+	}
+
+	public void land()
+	{
+		if (atcMediator.isLandingOk())
+		{
+			System.out.println("Successfully Landed.");
+			atcMediator.setLandingStatus(true);
+		}
+		else
+			System.out.println("Waiting for landing.");
+	}
+
+	public void getReady()
+	{
+		System.out.println("Ready for landing.");
+	}
+
+}
+
+class Runway implements Command
+{
+	private IATCMediator atcMediator;
+
+	public Runway(IATCMediator atcMediator)
+	{
+		this.atcMediator = atcMediator;
+		atcMediator.setLandingStatus(true);
+	}
+
+	@Override
+	public void land()
+	{
+		System.out.println("Landing permission granted.");
+		atcMediator.setLandingStatus(true);
+	}
+
+}
+
+class MediatorDesignPattern
+{
+	public static void main(String args[])
+	{
+
+		IATCMediator atcMediator = new ATCMediator();
+		Flight sparrow101 = new Flight(atcMediator);
+		Runway mainRunway = new Runway(atcMediator);
+		atcMediator.registerFlight(sparrow101);
+		atcMediator.registerRunway(mainRunway);
+		sparrow101.getReady();
+		mainRunway.land();
+		sparrow101.land();
+		
+	}
+}
+
+
+// Output:
+//Ready for landing.
+//Landing permission granted.
+//Successfully Landed.
