cb1d0ff1c0acae957a09987953b9c84645b9e5c8
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 11:43:19 +0100

    ISP code

--- SOLID/ISP/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ SOLID/ISP/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,50 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class HumanEmp implements Sleeper, Eater, Worker{
+    @Override
+    public void eat() {
+        System.out.println("Human is eating");
+    }
+    @Override
+    public void sleep() {
+        System.out.println("Human is sleeping");
+    }
+    @Override
+    public void work() {
+        System.out.println("Human is working");
+    }
+}
+
+public class RobotEmp implements  Worker{
+    @Override
+    public void work() {
+        System.out.println("Robot is working");
+    }
+}
+
+public interface Eater {
+    void eat();
+}
+
+public interface Sleeper {
+    void sleep();
+}
+
+public interface Worker {
+    void work();
+}
+
+public class App {
+    public static void main(String[] args) {
+        HumanEmp humanEmp = new HumanEmp();
+        RobotEmp robotEmp = new RobotEmp();
+        humanEmp.eat();
+        humanEmp.sleep();
+        humanEmp.work();
+        robotEmp.work();
     }
 }
