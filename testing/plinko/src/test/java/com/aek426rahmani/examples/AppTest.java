97886cd9dc8cff28eb3485c79f4ee7f2d4b036e9
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 10:56:39 +0100

    tests

--- testing/plinko/src/test/java/com/aek426rahmani/examples/AppTest.java
+++ testing/plinko/src/test/java/com/aek426rahmani/examples/AppTest.java
@@ -10,6 +10,42 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
+    // test for the getNumber method
+    public void testGetNumber() {
+        App app = new App();
+        int number = app.getNumber();
+        assertTrue(number > 0);
+    }
+    // test exception for the calculateFactoriel method
+    public void testCalculateFactoriel() {
+        App app = new App();
+        try {
+            int result = app.calculateFactoriel(-1);
+            assertTrue(result > 0);
+        } catch (NegativeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    // test for the calculateFactoriel method
+    public void testCalculateFactoriel2() {
+        App app = new App();
+        try {
+            int result = app.calculateFactoriel(5);
+            assertTrue(result > 0);
+        } catch (NegativeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    // test the factorial of 4
+    public void testCalculateFactoriel3() {
+        App app = new App();
+        try {
+            int result = app.calculateFactoriel(4);
+            assertTrue(result == 24);
+        } catch (NegativeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
     /**
      * Create the test case
      *
