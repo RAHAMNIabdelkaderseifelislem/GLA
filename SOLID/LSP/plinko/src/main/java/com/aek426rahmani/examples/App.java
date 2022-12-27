fdf0e24e3f6a9b6beedc61929b84f4eba20cfbbc
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 11:37:15 +0100

    LSP code

--- SOLID/LSP/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ SOLID/LSP/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,11 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
+
+// relize code that follows the LSP principle
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public abstract class App {
+    public abstract void info();
 }
---
+++ SOLID/LSP/plinko/src/main/java/com/aek426rahmani/examples/MyApp.java
@@ -0,0 +1,13 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
+
+package main.java.com.aek426rahmani.examples;
+
+public class MyApp extends App {
+    @Override
+    public void info() {
+        System.out.println("App built with Java and Maven as a build tool.");
+    }
+}
