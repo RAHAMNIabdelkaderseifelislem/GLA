ad743f395f81cccbee68287c0000bb6ef2338570
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 10:55:28 +0100

    application done

--- testing/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ testing/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,37 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
+/* simple application that's calculate the factoriel of an integer given by the user */
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Scanner;
+public class App {
+    Scanner scanner = new Scanner(System.in);
+    public int getNumber() {
+        System.out.print("Enter an integer: ");
+        int number = scanner.nextInt();
+        return number;
+    }
+    public int calculateFactoriel(int number) throws NegativeException {
+        if (number < 0) {
+            throw new NegativeException("The number is negative");
+            
+        } 
+        if (number ==0) {
+            return 1;
+        }
+        return number * calculateFactoriel(number - 1);
+    }
+
+    public static void main(String[] args) {
+        App app = new App();
+        int number = app.getNumber();
+        try {
+            int result = app.calculateFactoriel(number);
+            System.out.println("The factoriel of " + number + " is " + result);
+        } catch (NegativeException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
---
+++ testing/plinko/src/main/java/com/aek426rahmani/examples/NegativeException.java
@@ -0,0 +1,15 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
+/* this file is for handling the negative number exception */
+package com.aek426rahmani.examples;
+
+
+public class NegativeException extends Exception {
+
+    public NegativeException(String message) {
+        super(message);
+    }
+    
+}
