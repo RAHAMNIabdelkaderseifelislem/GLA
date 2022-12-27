b63db6d309b1021d653d2cb68fa775f834093155
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 18:02:34 +0100

    State code

--- Design_Patterns/Behavior/State/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ Design_Patterns/Behavior/State/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,77 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
+// source : https://www.geeksforgeeks.org/state-design-pattern/
+
+// Java program to demonstrate working of
+// State Design Pattern
+
+interface MobileAlertState
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+	public void alert(AlertStateContext ctx);
 }
+
+class AlertStateContext
+{
+	private MobileAlertState currentState;
+
+	public AlertStateContext()
+	{
+		currentState = new Vibration();
+	}
+
+	public void setState(MobileAlertState state)
+	{
+		currentState = state;
+	}
+
+	public void alert()
+	{
+		currentState.alert(this);
+	}
+}
+
+class Vibration implements MobileAlertState
+{
+	@Override
+	public void alert(AlertStateContext ctx)
+	{
+		System.out.println("vibration...");
+	}
+
+}
+
+class Silent implements MobileAlertState
+{
+	@Override
+	public void alert(AlertStateContext ctx)
+	{
+		System.out.println("silent...");
+	}
+
+}
+
+class StatePattern
+{
+	public static void main(String[] args)
+	{
+		AlertStateContext stateContext = new AlertStateContext();
+		stateContext.alert();
+		stateContext.alert();
+		stateContext.setState(new Silent());
+		stateContext.alert();
+		stateContext.alert();
+		stateContext.alert();		
+	}
+}
+
+// Output:
+
+//vibration...
+//vibration...
+//silent...
+//silent...
+//silent...
