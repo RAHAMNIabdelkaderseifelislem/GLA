b0ade0cca2a1cdfff50c63d3c67987052f31e47c
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 16:50:45 +0100

    factory add some comments

--- Design_Patterns/Creation/Factory/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ Design_Patterns/Creation/Factory/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -69,3 +69,17 @@ public class NotificationService {
 	}
 }
 
+
+/*
+ * Real-time examples:
+
+This design pattern has been widely used in JDK, such as 
+
+1. getInstance() method of java.util.Calendar, NumberFormat, and ResourceBundle 
+        uses factory method design pattern. 
+2. All the wrapper classes like Integer, Boolean etc, in Java uses this pattern 
+        to evaluate the values using valueOf() method. 
+3. java.nio.charset.Charset.forName(), java.sql.DriverManager#getConnection(), 
+        java.net.URL.openConnection(), java.lang.Class.newInstance(), java.lang.Class.forName() 
+        are some of their example where factory method design pattern has been used.
+ */
