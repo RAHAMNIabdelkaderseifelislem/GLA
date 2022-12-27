224d0e14475e4f88012d0366359e683de0b79053
RAHAMNIabdelkaderseifelislem <a.e.k426rahmani@gmail.com>
2022-12-27 17:38:40 +0100

    COR code

--- Design_Patterns/Behavior/ChainOfResponsibility/plinko/src/main/java/com/aek426rahmani/examples/App.java
+++ Design_Patterns/Behavior/ChainOfResponsibility/plinko/src/main/java/com/aek426rahmani/examples/App.java
@@ -1,13 +1,134 @@
+/*
+ * created by: aek426rahmani
+ * date: 27-12-2022
+ */
 package com.aek426rahmani.examples;
 
-/**
- * Hello world!
- *
- */
-public class App 
+// source : https://www.geeksforgeeks.org/chain-responsibility-design-pattern/
+
+public class Chain
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+Processor chain;
+
+public Chain(){
+	buildChain();
 }
+
+private void buildChain(){
+	chain = new NegativeProcessor(new ZeroProcessor(new PositiveProcessor(null)));
+}
+
+public void process(Number request) {
+	chain.process(request);
+}
+
+}
+
+abstract class Processor
+{
+	private Processor nextProcessor;
+
+	public Processor(Processor nextProcessor){
+		this.nextProcessor = nextProcessor;
+	};
+	
+	public void process(Number request){
+		if(nextProcessor != null)
+			nextProcessor.process(request);
+	};
+}
+
+class Number
+{
+	private int number;
+
+	public Number(int number)
+	{
+		this.number = number;
+	}
+
+	public int getNumber()
+	{
+		return number;
+	}
+
+}
+
+class NegativeProcessor extends Processor
+{
+	public NegativeProcessor(Processor nextProcessor){
+		super(nextProcessor);
+		
+	}
+
+	public void process(Number request)
+	{
+		if (request.getNumber() < 0)
+		{
+			System.out.println("NegativeProcessor : " + request.getNumber());
+		}
+		else
+		{
+			super.process(request);
+		}
+	}
+}
+
+class ZeroProcessor extends Processor
+{
+	public ZeroProcessor(Processor nextProcessor){
+		super(nextProcessor);
+	}
+
+	public void process(Number request)
+	{
+		if (request.getNumber() == 0)
+		{
+			System.out.println("ZeroProcessor : " + request.getNumber());
+		}
+		else
+		{
+			super.process(request);
+		}
+	}
+}
+
+class PositiveProcessor extends Processor
+{
+
+	public PositiveProcessor(Processor nextProcessor){
+		super(nextProcessor);
+	}
+
+	public void process(Number request)
+	{
+		if (request.getNumber() > 0)
+		{
+			System.out.println("PositiveProcessor : " + request.getNumber());
+		}
+		else
+		{
+			super.process(request);
+		}
+	}
+}
+
+class TestChain
+{
+	public static void main(String[] args) {
+		Chain chain = new Chain();
+		
+		//Calling chain of responsibility
+		chain.process(new Number(90));
+		chain.process(new Number(-50));
+		chain.process(new Number(0));
+		chain.process(new Number(91));
+	}
+}
+
+// Output :
+
+//PositiveProcessor : 90
+//NegativeProcessor : -50
+//ZeroProcessor : 0
+//PositiveProcessor : 91
